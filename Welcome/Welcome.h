636d7cc9bc911356037c4597a8962b94ab27daa1
wlyanlei <wlyanlei@myseneca.ca>
2024-07-26 22:16:01 -0400

    Add Welcome project
    Hello World

---
+++ Welcome/Welcome.h
@@ -0,0 +1,22 @@
+/**
+********************************************************************************
+ * Author: Wei Liang Yan Lei
+ * Starting date: Friday, July 26th, 2024 around 7:40 pm.
+ * Completion date: the same day at around 7:50 pm.
+ * Purpose: Display welcome message to everyone who joined the WhatsApp community
+ ********************************************************************************
+**/
+
+#ifndef SENECA_WELCOME_H
+#define SENECA_WELCOME_H
+
+namespace Seneca {
+
+    /**
+     * Display a warm welcoming string of characters to the `stdout` stream.
+     */
+    void displayWelcomeMessage();
+
+}
+
+#endif
---
+++ Welcome/main.cpp
@@ -0,0 +1,18 @@
+/**
+ ********************************************************************************
+ * Author: Wei Liang Yan Lei
+ * Starting date: Friday, July 26th, 2024 around 7:40 pm.
+ * Completion date: the same day at around 7:50 pm.
+ * Purpose: Display welcome message to everyone who joined the WhatsApp community
+ ********************************************************************************
+**/
+
+#include <iostream>
+
+#include "Welcome.h"
+
+int main()
+{
+    Seneca::displayWelcomeMessage();
+    return 0;
+}
